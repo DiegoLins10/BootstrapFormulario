3cdb080bff23588fb9d453c7f80376262b86af1b
Diego Lins <68483773+DiegoLins10@users.noreply.github.com>
2021-03-13 17:03:29 -0300

    formulario bootstrap 13/03/21

---
+++ src/css/personalizado.css
@@ -0,0 +1,227 @@
+.btn-listar {
+  color: #fff;
+  background-color: #E81504;
+  border-color: #E82800;
+}
+.btn-listar:focus,
+.btn-listar.focus {
+  color: #fff;
+  background-color: #FF211D;
+  border-color: #E82800;
+}
+.btn-listar:hover {
+  color: #fff;
+  background-color: #FF211D;
+  border-color: #E82800;
+}
+.btn-listar:active,
+.btn-listar.active,
+.open > .dropdown-toggle.btn-listar {
+  color: #fff;
+  background-color: #FF211D;
+  border-color: #E82800;
+}
+.btn-listar:active:hover,
+.btn-listar.active:hover,
+.open > .dropdown-toggle.btn-listar:hover,
+.btn-listar:active:focus,
+.btn-listar.active:focus,
+.open > .dropdown-toggle.btn-listar:focus,
+.btn-listar:active.focus,
+.btn-listar.active.focus,
+.open > .dropdown-toggle.btn-listar.focus {
+  color: #fff;
+  background-color: #E82800;
+  border-color: #E82800;
+}
+.btn-listar:active,
+.btn-listar.active,
+.open > .dropdown-toggle.btn-listar {
+  background-image: none;
+}
+.btn-listar.disabled:hover,
+.btn-listar[disabled]:hover,
+fieldset[disabled] .btn-listar:hover,
+.btn-listar.disabled:focus,
+.btn-listar[disabled]:focus,
+fieldset[disabled] .btn-listar:focus,
+.btn-listar.disabled.focus,
+.btn-listar[disabled].focus,
+fieldset[disabled] .btn-listar.focus {
+  background-color: #E81504;
+  border-color: #E82800;
+}
+.btn-listar .badge {
+  color: #E81504;
+  background-color: #fff;
+}
+
+
+
+.btn-relatorio {
+  color: #fff;
+  background-color: #6D0FE8;
+  border-color: #9703E8;
+}
+.btn-relatorio:focus,
+.btn-relatorio.focus {
+  color: #fff;
+  background-color: #722EFF;
+  border-color: #9703E8;
+}
+.btn-relatorio:hover {
+  color: #fff;
+  background-color: #722EFF;
+  border-color: #9703E8;
+}
+.btn-relatorio:active,
+.btn-relatorio.active,
+.open > .dropdown-toggle.btn-relatorio {
+  color: #fff;
+  background-color: #722EFF;
+  border-color: #9703E8;
+}
+.btn-relatorio:active:hover,
+.btn-relatorio.active:hover,
+.open > .dropdown-toggle.btn-relatorio:hover,
+.btn-relatorio:active:focus,
+.btn-relatorio.active:focus,
+.open > .dropdown-toggle.btn-relatorio:focus,
+.btn-relatorio:active.focus,
+.btn-relatorio.active.focus,
+.open > .dropdown-toggle.btn-relatorio.focus {
+  color: #fff;
+  background-color: #9703E8;
+  border-color: #9703E8;
+}
+.btn-relatorio:active,
+.btn-relatorio.active,
+.open > .dropdown-toggle.btn-relatorio {
+  background-image: none;
+}
+.btn-relatorio.disabled:hover,
+.btn-relatorio[disabled]:hover,
+fieldset[disabled] .btn-relatorio:hover,
+.btn-relatorio.disabled:focus,
+.btn-relatorio[disabled]:focus,
+fieldset[disabled] .btn-relatorio:focus,
+.btn-relatorio.disabled.focus,
+.btn-relatorio[disabled].focus,
+fieldset[disabled] .btn-relatorio.focus {
+  background-color: #6D0FE8;
+  border-color: #9703E8;
+}
+.btn-relatorio .badge {
+  color: #6D0FE8;
+  background-color: #fff;
+}
+
+
+
+
+
+.btn-relatorioexcel {
+  color: #fff;
+  background-color: #68FF0B;
+  border-color: #86E80D;
+}
+.btn-relatorioexcel:focus,
+.btn-relatorioexcel.focus {
+  color: #fff;
+  background-color: #BEFF18;
+  border-color: #86E80D;
+}
+.btn-relatorioexcel:hover {
+  color: #fff;
+  background-color: #BEFF18;
+  border-color: #86E80D;
+}
+.btn-relatorioexcel:active,
+.btn-relatorioexcel.active,
+.open > .dropdown-toggle.btn-relatorioexcel {
+  color: #fff;
+  background-color: #BEFF18;
+  border-color: #86E80D;
+}
+.btn-relatorioexcel:active:hover,
+.btn-relatorioexcel.active:hover,
+.open > .dropdown-toggle.btn-relatorioexcel:hover,
+.btn-relatorioexcel:active:focus,
+.btn-relatorioexcel.active:focus,
+.open > .dropdown-toggle.btn-relatorioexcel:focus,
+.btn-relatorioexcel:active.focus,
+.btn-relatorioexcel.active.focus,
+.open > .dropdown-toggle.btn-relatorioexcel.focus {
+  color: #fff;
+  background-color: #86E80D;
+  border-color: #86E80D;
+}
+.btn-relatorioexcel:active,
+.btn-relatorioexcel.active,
+.open > .dropdown-toggle.btn-relatorioexcel {
+  background-image: none;
+}
+.btn-relatorioexcel.disabled:hover,
+.btn-relatorioexcel[disabled]:hover,
+fieldset[disabled] .btn-relatorioexcel:hover,
+.btn-relatorioexcel.disabled:focus,
+.btn-relatorioexcel[disabled]:focus,
+fieldset[disabled] .btn-relatorioexcel:focus,
+.btn-relatorioexcel.disabled.focus,
+.btn-relatorioexcel[disabled].focus,
+fieldset[disabled] .btn-relatorioexcel.focus {
+  background-color: #68FF0B;
+  border-color: #86E80D;
+}
+.btn-relatorioexcel .badge {
+  color: #68FF0B;
+  background-color: #fff;
+}
+
+
+
+.btn-pdf { 
+  color: #ffffff; 
+  background-color: #1BBDAF; 
+  border-color: #026947; 
+} 
+ 
+.btn-pdf:hover, 
+.btn-pdf:focus, 
+.btn-pdf:active, 
+.btn-pdf.active, 
+.open .dropdown-toggle.btn-pdf { 
+  color: #ffffff; 
+  background-color: #026947; 
+  border-color: #026947; 
+} 
+ 
+.btn-pdf:active, 
+.btn-pdf.active, 
+.open .dropdown-toggle.btn-pdf { 
+  background-image: none; 
+} 
+ 
+.btn-pdf.disabled, 
+.btn-pdf[disabled], 
+fieldset[disabled] .btn-pdf, 
+.btn-pdf.disabled:hover, 
+.btn-pdf[disabled]:hover, 
+fieldset[disabled] .btn-pdf:hover, 
+.btn-pdf.disabled:focus, 
+.btn-pdf[disabled]:focus, 
+fieldset[disabled] .btn-pdf:focus, 
+.btn-pdf.disabled:active, 
+.btn-pdf[disabled]:active, 
+fieldset[disabled] .btn-pdf:active, 
+.btn-pdf.disabled.active, 
+.btn-pdf[disabled].active, 
+fieldset[disabled] .btn-pdf.active { 
+  background-color: #1BBDAF; 
+  border-color: #026947; 
+} 
+ 
+.btn-pdf .badge { 
+  color: #1BBDAF; 
+  background-color: #ffffff; 
+}
